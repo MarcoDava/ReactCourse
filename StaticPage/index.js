4d7680ecc2e76c2c5140a2e024f3bab75aaf4cd8
MarcoDava <marco.a.dava@gmail.com>
2025-04-17 12:27:11 -0400

    Creating a static page in react:

---
+++ StaticPage/index.js
@@ -0,0 +1,13 @@
+function MainContent(){
+    return(
+        <nav className="main-content paragraph">
+            <p>something? idk lol</p>
+        </nav>
+    )
+}
+
+ReactDOM.render(<ul>
+    <li>Hello</li>
+    <li>World</li>
+</ul>
+, document.getElementById("root"))
